e66daa88130fb7230e2c42f1f8e3abb87b7c0f4b
Łukasz Darmochwał <tcpvg@o2.pl>
2021-03-22 17:11:55 +0100

    link do api + pakiety do jsona

---
+++ BozaPodroz/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BozaPodroz.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "2SLIARQQVP1BCJ2S1MHIKTOXE53NRGLEHVQL3TTAKP34WUIA";
+        public const string CLIENT_SECRET = "AGWRBAZBJ0MP2P4IU1AN1BZ0PMWHSO5ARCDOGO15BJFD5Q4O";
+    }
+}
---
+++ BozaPodroz/Model/Venue.cs
@@ -0,0 +1,15 @@
+﻿using BozaPodroz.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BozaPodroz.Model
+{
+    public class Venue
+    {
+        public static string GenerateURL(double latitude, double longitude)
+        {
+           return string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID, Constants.CLIENT_SECRET,DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
