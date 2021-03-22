e35ac16f2b70d75f9e8c186be919299081170d7e
Łukasz Darmochwał <tcpvg@o2.pl>
2021-03-22 15:14:13 +0100

    Zmiany na mapie co 100 metrów + zaktualizowanie nugetów

--- BozaPodroz/MapPage.xaml.cs
+++ BozaPodroz/MapPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Plugin.Permissions;
+﻿using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,7 @@ namespace BozaPodroz
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        private bool hasLocationPermission = false;
         public MapPage()
         {
             InitializeComponent();
@@ -39,10 +42,53 @@ namespace BozaPodroz
                 if (status == PermissionStatus.Granted)
                 {
                     locationsMap.IsShowingUser = true;
+                    hasLocationPermission = true;
+
+                    GetLocation();
                 }
                 else DisplayAlert("bez lokalizacji nie ma aplikacji", "hehe", "ok");
             }
             catch(Exception ex) { DisplayAlert("error", ex.Message, "ok"); };
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                locator.PositionChanged += Locator_PositionChanged;
+                await locator.StartListeningAsync(TimeSpan.Zero, 100);
+            }
+
+            GetLocation();
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            CrossGeolocator.Current.StopListeningAsync();
+            CrossGeolocator.Current.PositionChanged -= Locator_PositionChanged;
+        }
+        void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
+        {
+            MoveMap(e.Position);
+        }
+        private async void GetLocation()
+        {
+
+            if (hasLocationPermission)
+            {
+                var locator = CrossGeolocator.Current;
+                var position = await locator.GetPositionAsync();
+                MoveMap(position);
+            }
+        }
+        private void MoveMap(Position position)
+        {
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(center, 1, 1);
+            locationsMap.MoveToRegion(span);
         }
     }
 }
