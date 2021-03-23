20b61ebf44ad357b644501d4b2099b31c253f8d5
Łukasz Darmochwał <tcpvg@o2.pl>
2021-03-23 21:02:23 +0100

    Stworzenie profile page(LINQ) i koniec

--- BozaPodroz/ProfilePage.xaml.cs
+++ BozaPodroz/ProfilePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BozaPodroz.Model;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,36 @@ namespace BozaPodroz
         public ProfilePage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+             
+            using(SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+                var categories = (from p in postTable
+                                  orderby p.CategoryId descending
+                                  where p.CategoryName != null
+                                  select p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                    foreach (var category in categories)
+                    {
+                        var count = (from post in postTable
+                                     where post.CategoryName == category
+                                     select post).ToList().Count;
+
+                        var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count;
+                        categoriesCount.Add(category, count);
+                    }
+                
+                    categoriesListView.ItemsSource = categoriesCount;
+                
+
+                postCountLabel.Text = postTable.Count.ToString();
+            }
         }
     }
 }
